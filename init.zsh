6ff71192d50db5d6fb9f1e4882e9b021775e0e40
Taylor Hardy <hardy.taylor.j@gmail.com>
2017-09-02 11:49:12 -0400

    initial commit

---
+++ init.zsh
@@ -0,0 +1,24 @@
+
+alias ya='yaourt'
+alias yai='yaourt -S'
+alias yar='yaourt -R'
+alias yap='yaourt -Rs'
+alias yas='yaourt -Ss'
+alias yasa='yaourt -Ssa'
+alias yad='yaourt -Si'
+alias yay='sudo yaourt -Sy'
+alias yayu='sudo yaourt -Syu'
+alias yau='sudo yaourt -Su'
+
+alias yah='yaourt -h; echo "
+Yaourt Aliases: corresponding letters in description capitalized for mneumonic help ;)
+ya: YAourt
+yah: show this Help
+yai: Install
+yar: Remove
+yap: Purge
+yas: Search
+yad: show (show Details of a package)
+yay: update (sYnchronize)
+yau: Upgrade
+yayu: update (sYnchronize) -> Upgrade"'
